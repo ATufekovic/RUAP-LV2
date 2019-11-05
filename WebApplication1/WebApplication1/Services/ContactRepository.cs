b54c727f12d48139c1334c6155b9131befd35a2f
ATufekovic <atufekovic@gmail.com>
2019-11-05 17:44:32 +0100

    Task 4 - Extracting functionality into a service layer

---
+++ WebApplication1/WebApplication1/Services/ContactRepository.cs
@@ -0,0 +1,23 @@
+﻿using WebApplication1.Models;
+namespace WebApplication1.Services
+{
+    public class ContactRepository
+    {
+        public Contact[] GetAllContacts()
+        {
+            return new Contact[]
+            {
+                new Contact
+                {
+                    Id = 1,
+                    Name = "Glenn great Block"
+                },
+                new Contact
+                {
+                    Id = 2,
+                    Name = "Dan Roth"
+                }
+            };
+        }
+    }
+}
